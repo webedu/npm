7e9c99ce650b8821b9294bf3fc33865390eeb6ac
Unknown <michael.kahle@yahoo.de>
2019-04-16 18:30:37 +0200

    Now check if parent-child relation is really nested

--- packages/C4uGlue/src/C4uGlue.js
+++ packages/C4uGlue/src/C4uGlue.js
@@ -26,7 +26,7 @@ const glueStore = new Vuex.Store({
        }
        // remove current id if allready inside the list
        state.c4uCurrentIds[elem.c4uTag] = state.c4uCurrentIds[elem.c4uTag].filter(
-          function (e, i, array) { return e != elem.c4uTag; }
+          function (e, i, array) { return e != elem.c4uUid; }
        );
        // current id gets last element
        state.c4uCurrentIds[elem.c4uTag].push(elem.c4uUid);
@@ -36,6 +36,26 @@ const glueStore = new Vuex.Store({
 
 let ucid = 1;
 
+function checkifChildBelowParentDOM(child, parent)
+{
+   for(var i = 0; i < parent.children.length; i++) {
+      var elem = parent.children[i];
+      if (elem._wrapper && elem._wrapper.$el && elem._wrapper.$el.id == child.id) {
+         return true;
+      }
+      if (checkifChildBelowParentDOM(child, elem)) {
+         return true;
+      } 
+   } 
+   return false;
+}
+
+function checkIfChildBelowParentVue(child, parent)
+{
+  if (!child.$root.$el || !parent.$root.$el) {return false;}
+  return checkifChildBelowParentDOM(child.$root.$el, parent.$root.$el);
+}
+
 export default {
    data: function() {
       return {
@@ -44,7 +64,8 @@ export default {
         c4uParentId: -1,
         c4uUid: -1,
         c4uTag: "",
-        c4uParent: null,                    // the parent component  
+        c4uParent: null,                    // the parent component 
+        c4uAncestor: null,                  // the ancestor component (similiar to parent, but maybe not nested) 
         c4uChildren: {},                    // list of components per tag
         }
     },
@@ -53,7 +74,8 @@ export default {
             //console.log("ENtry received "+child.c4uParentId+" / "+ this.c4uUid+" / "+ child.c4uUid);
             if (this.c4uUid == child.c4uParentId) {
                 //console.log(" ENtry accepted "+child.c4uParentId+" / "+ this.c4uUid+" / "+ child.c4uUid);
-                child.c4uParent = this;               
+                child.c4uParent = this;     // maybe get changed later (if mounted)
+                child.c4uAncestor = this;   // this is final here           
                 var childTag = child.c4uTag;
                 var uniqueElements = new Array();
                 if(childTag in this.c4uChildren) {
@@ -69,6 +91,41 @@ export default {
                 this.c4uChildren[childTag] = newChildren;
             }
           }, 
+          c4uRecheckChild(child) {
+             var childTag = child.c4uTag;
+             var isInside = checkIfChildBelowParentVue(child, this);
+             // remove from array
+             if(childTag in this.c4uChildren) {
+               this.c4uChildren[childTag] = this.c4uChildren[childTag].filter(
+                 function (e, i, array) { return e.c4uUid != child.c4uUid; }
+               );
+             }
+             if (isInside) {
+                //console.log("Child inside "+ this.c4uUid+" / "+ child.c4uUid);
+                child.c4uParent = this;
+                child.c4uParentId = this.c4uUid;
+                // add child to list
+                var uniqueElements = new Array();
+                if(childTag in this.c4uChildren) {
+                  this.c4uChildren[childTag].forEach(elem => {
+                    uniqueElements[elem.c4uUid] = elem;
+                  })
+                }
+                uniqueElements[child.c4uUid] = child;
+                var newChildren = new Array();
+                for (var items in uniqueElements){
+                  newChildren.push( uniqueElements[items] );
+                }
+                this.c4uChildren[childTag] = newChildren;
+             } else {
+                // already removed from list - so only reset child if necassary.  
+                //console.log("Child outside "+ this.c4uUid+" / "+ child.c4uUid);
+                if(child.c4uParentId == this.c4uUid) {
+                  child.c4uParent = null;
+                  child.c4uParentId = -1;
+                } 
+             }
+          },
     },
 
     created() {
@@ -78,17 +135,26 @@ export default {
          }
          this.c4uTag = this.$root.$options.customElement.localName; 
          // receive/handle requests from child as parent 
-         glueBus.$on('c4u-add+child-'+this.c4uTag, (v1) => this.c4uAddChild(v1));
+         glueBus.$on('c4u-add-child-'+this.c4uTag, (v1) => this.c4uAddChild(v1));
          glueStore.commit('c4uAddCurrentUid', this);  
          // send request as child to parent
          if (this.c4uParentId < 0) {
            // might better get list and check (inside event) until parent really has this child
            // but this check is only possible on parent.root.el.childNodes -> mounted
            this.c4uParentId = glueStore.getters.c4UGetLastUid(this.c4uParentTag); // last, might not real parent, but ancestor 
-           glueBus.$emit('c4u-add+child-'+this.c4uParentTag, this);
+           glueBus.$emit('c4u-add-child-'+this.c4uParentTag, this);
          }
     }, 
     mounted() {
          this.$root.$el.id = 'c4uid-' + this.c4uUid;
+         this.c4uTag = this.$root.$options.customElement.localName; 
+         // receive/handle requests from child as parent 
+         glueBus.$on('c4u-check-child-'+this.c4uTag, (v1) => this.c4uRecheckChild(v1));
+         this.$nextTick( function () { 
+            glueBus.$emit('c4u-check-child-'+this.c4uParentTag, this);
+         });
+         
+         
+         
     },
 };
